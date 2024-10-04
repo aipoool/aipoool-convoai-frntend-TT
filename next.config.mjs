c983763deef4ffbc965e1cbe08c9dbd3a89f1948
aipoool <comdtvise@gmail.com>
2024-10-04 19:13:52 +0530

    chore: Updated the routing part

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    async redirects() {
+      return [
+        {
+          source: '/old-page',
+          destination: '/new-page',
+          permanent: true,
+        },
+        // Add more redirect rules as needed
+      ]
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://api.example.com/:path*',
+        },
+        // Add more rewrite rules as needed
+      ]
+    },
+  }
+  
+  module.exports = nextConfig
